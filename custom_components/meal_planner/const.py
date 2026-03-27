afa6cc128a63804f16c6b22f2a5aef85a1ba12ab
HACucoo <152744401+HACucoo@users.noreply.github.com>
2026-03-27 18:09:54 +0100

    Update README.md
    Screenshot

---
+++ custom_components/meal_planner/const.py
@@ -0,0 +1,52 @@
+DOMAIN = "meal_planner"
+STORAGE_KEY = "meal_planner"
+STORAGE_VERSION = 1
+
+PANEL_URL = "meal-planner"
+PANEL_TITLE = "Meal Planner"
+PANEL_ICON = "mdi:silverware-fork-knife"
+
+DEFAULT_DISHES = [
+    "Spaghetti Bolognese",
+    "Hähnchen-Curry",
+    "Gemüsepfanne",
+    "Schnitzel mit Pommes",
+    "Pizza Margherita",
+    "Lasagne",
+    "Gulasch",
+    "Rinderbraten",
+    "Tortellini in Tomatensoße",
+    "Griechischer Salat mit Fladenbrot",
+    "Tacos",
+    "Burger",
+    "Gebratener Reis",
+    "Nudeln mit Pesto",
+    "Shakshuka",
+    "Linsensuppe",
+    "Minestrone",
+    "Flammkuchen",
+    "Käsespätzle",
+    "Chili con Carne",
+    "Wraps mit Hähnchen",
+    "Fischstäbchen mit Kartoffelbrei",
+    "Grillgemüse mit Couscous",
+    "Risotto",
+    "Gnocchi in Salbeibutter",
+    "Pfannkuchen",
+    "Ofenkartoffeln",
+    "Quiche Lorraine",
+    "Gemüsesuppe",
+    "Ramen",
+    "Pad Thai",
+    "Pulled Pork",
+    "Lachsfilet mit Reis",
+    "Chicken Tikka Masala",
+    "Burritos",
+]
+
+# Day plan types
+TYPE_DISH = "dish"
+TYPE_EATING_OUT = "eating_out"
+TYPE_ORDER = "order"
+TYPE_NOTHING = "nothing"
+TYPE_CUSTOM = "custom"
